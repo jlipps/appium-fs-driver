5d2c77c270b8fc001b96c7a7c4daea3cee64d989
Jonathan Lipps <jlipps@gmail.com>
2021-09-13 18:15:04 -0700

    get an empty driver running

--- src/driver.js
+++ src/driver.js
@@ -1,4 +1,6 @@
-class AppiumFSDriver {
+import { BaseDriver } from '@appium/base-driver'
+
+class AppiumFSDriver extends BaseDriver {
 }
 
 export { AppiumFSDriver }
--- src/test/driver.test.js
+++ src/test/driver.test.js
@@ -1,7 +1,45 @@
 /** @type {import('@types/jest')} */
+/** @type {import('webdriverio').remote} */
 
 import { AppiumFSDriver } from '../driver'
 
+import { remote } from 'webdriverio'
+
+// TODO for now we assume a running Appium server with our driver linked in, but to make this
+// a real robust test suite, we'd want to dynamically run an Appium 2.0 server instead of relying
+// on an external one
+const APPIUM_HOST = 'localhost'
+const APPIUM_PORT = 4723
+
 test('driver export exists', () => {
   expect(AppiumFSDriver).toBeDefined()
+})
+
+describe('E2E', () => {
+  const capabilities = {
+    platformName: 'Mac',
+    'appium:automationName': 'FS',
+  }
+  const params = {
+    connectionRetryCount: 0,
+    hostname: APPIUM_HOST,
+    port: APPIUM_PORT,
+    path: '/',
+    capabilities
+  }
+
+  let driver = null
+
+  beforeAll(async () => {
+    driver = await remote(params)
+  })
+
+  test('do something', () => {
+  })
+
+  afterAll(async () => {
+    if (driver) {
+      await driver.deleteSession()
+    }
+  })
 })
