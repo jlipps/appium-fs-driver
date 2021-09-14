8c10ff79a046a69a5e234d22cacddd1188eeff36
Jonathan Lipps <jlipps@gmail.com>
2021-09-14 15:38:47 -0700

    add getPageSource command

--- src/driver.js
+++ src/driver.js
@@ -1,6 +1,17 @@
+import path from 'path'
+import { fs } from '@appium/support'
 import { BaseDriver } from '@appium/base-driver'
 
 class AppiumFSDriver extends BaseDriver {
+
+  desiredCapConstraints = {baseDir: {presence: true}}
+
+  async getPageSource() {
+    // TODO return an actual directory hierarchy instead of a flat list
+    const files = await fs.glob(path.resolve(this.opts.baseDir, '**', '*'))
+    const fileNodes = files.map((filePath) => `\t<file path="${filePath}" />`)
+    return `<files>\n${fileNodes.join('\n')}</files>`
+  }
 }
 
 export { AppiumFSDriver }
--- src/test/driver.test.js
+++ src/test/driver.test.js
@@ -1,8 +1,8 @@
 /** @type {import('@types/jest')} */
 /** @type {import('webdriverio').remote} */
 
+import path from 'path'
 import { AppiumFSDriver } from '../driver'
-
 import { remote } from 'webdriverio'
 
 // TODO for now we assume a running Appium server with our driver linked in, but to make this
@@ -10,31 +10,49 @@ import { remote } from 'webdriverio'
 // on an external one
 const APPIUM_HOST = 'localhost'
 const APPIUM_PORT = 4723
+const BASE_DIR = path.resolve(__dirname, '..')
+const WDIO_PARAMS = {
+  connectionRetryCount: 0,
+  hostname: APPIUM_HOST,
+  port: APPIUM_PORT,
+  path: '/',
+  logLevel: 'silent',
+}
+const DEFAULT_CAPS = {
+  platformName: 'Mac',
+  'appium:automationName': 'FS',
+  'appium:baseDir': BASE_DIR,
+}
 
 test('driver export exists', () => {
   expect(AppiumFSDriver).toBeDefined()
 })
 
-describe('E2E', () => {
-  const capabilities = {
-    platformName: 'Mac',
-    'appium:automationName': 'FS',
-  }
-  const params = {
-    connectionRetryCount: 0,
-    hostname: APPIUM_HOST,
-    port: APPIUM_PORT,
-    path: '/',
-    capabilities
-  }
+describe('E2E - session start', () => {
+  test('should not start a session without baseDir cap', async () => {
+    const capabilities = {
+      platformName: 'Mac',
+      'appium:automationName': 'FS',
+    }
+    let err
+    try {
+      await remote({...WDIO_PARAMS, capabilities})
+    } catch (e) {
+      err = e
+    }
+    expect(err.message).toMatch(/baseDir/)
+  })
+})
 
+describe('E2E - commands', () => {
   let driver = null
 
   beforeAll(async () => {
-    driver = await remote(params)
+    driver = await remote({...WDIO_PARAMS, capabilities: DEFAULT_CAPS})
   })
 
-  test('do something', () => {
+  test('should get directory listing as page source', async () => {
+    expect(await driver.getPageSource()).toContain(__filename)
   })
 
   afterAll(async () => {
